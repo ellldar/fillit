103eed4a1e2e004fcf32c3b648b7172d23e683ec
esupatae <esupatae@student.42.us.org>
2020-01-17 22:41:44 -0800

    All errors fixed - 5 norminette errors remaining

---
+++ libft/ft_atoi_base.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esupatae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/14 00:38:46 by esupatae          #+#    #+#             */
+/*   Updated: 2019/11/14 00:38:54 by esupatae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_ok_char(char c)
+{
+	static int	chrs[] = {0, 9, 10, 11, 12, 13, 32};
+	int			i;
+
+	i = 0;
+	while (i++ < 7)
+		if (c == (char)chrs[i - 1])
+			return (1);
+	return (0);
+}
+
+static int	is_valid_char(char c)
+{
+	static char	*letters = "0123456789abcdef";
+	size_t		i;
+
+	i = 0;
+	while (i++ < ft_strlen(letters))
+		if (ft_tolower(c) == letters[i - 1])
+			return (i);
+	return (0);
+}
+
+int			ft_atoi_base(const char *str, int str_base)
+{
+	int	result;
+	int	is_negative;
+	int v;
+
+	result = 0;
+	while (is_ok_char(*str))
+		str++;
+	is_negative = *str == '-' ? -1 : 1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && (v = is_valid_char(*str++)))
+		if ((v - 1) < str_base)
+			result = (result * str_base) + (v - 1);
+		else
+			break ;
+	return (result * is_negative);
+}
